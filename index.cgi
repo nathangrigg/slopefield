d44ee27233bfe81588306133931a5a6a6a9a552a
Nathan Grigg <nathan@nathanamy.org>
2012-03-22 11:25:58 -0700

    Refactor: moving most logic from cgi to module

--- index.cgi
+++ index.cgi
@@ -3,10 +3,10 @@
 import cgi
 import sys
 
-storage = cgi.FieldStorage()
+cgi_input = cgi.FieldStorage()
 
 # if there are no request parameters, write out static file, if possible
-if len(storage.keys()) == 0:
+if len(cgi_input.keys()) == 0 and len(sys.argv)==1:
     try:
         f = open('landing.html','r')
     except:
@@ -20,188 +20,10 @@ if len(storage.keys()) == 0:
             sys.exit()
 
 import slopefield
-import re
 import cgitb
-from math import sin,cos,tan,sqrt,e,pi,log,cosh,sinh,tanh
-ln = log
-
-VALID_WORDS = ['','sin','cos','tan','t','y','abs','sqrt','e','pi','log','ln',
-    'acos','asin','atan','cosh','sinh','tanh']
-
-# Errors will be logged to LOG_FILE
-LOG_FILE = ""
-
-def cgi_get(name,default,convert=float):
-    """Gets the cgi value and converts it, with a default value on error"""
-    field_str = storage.getfirst(name,str(default))
-    try:
-        value = convert(field_str)
-    except ValueError:
-        value = default
-
-    return value
-
-def clip(value,left,right):
-    """Forces value to be within the range [left, right]"""
-    return min(right,max(left,value))
-
-def sanitize(fn_str):
-    """Sanitizes fn_str, evaluates, and returns a function"""
-    words = re.split(r'[0-9.+\-*/^ ()]+',fn_str)
-    for word in words:
-         if word not in VALID_WORDS:
-            error('Unrecognized expression in function: %s' % word)
-
-    s = fn_str.replace('^','**')
-
-    # replace 1.232 with float(1.234)
-    s = re.sub(r'[0-9.]+', r'float(\g<0>)', s)
-
-    try:
-        fn = eval("lambda t,y: " + s)
-    except SyntaxError:
-        error('Syntax Error. Something is wrong with the function you entered.\
-        Common mistakes include writing <tt>3x</tt> instead of <tt>3*x</tt> or\
-        <tt>sin t</tt> instead of <tt>sin(t)</tt>.')
-    except NameError:
-        write_log(fn_str,sys.exc_info(),'eval')
-        error('Undefined function: %s' % sys.exc_info()[1])
-    except:
-        write_log(fn_str,sys.exc_info(),'eval')
-        error('Something is wrong with the function you entered.')
-
-    # sanity check
-    try:
-        output = fn(1.25,1.25)
-    except ValueError:
-        pass
-    except ZeroDivisionError:
-        pass
-    except OverflowError:
-        pass
-    except:
-        write_log(fn_str,sys.exc_info(),'sanity check')
-        error('Something is wrong with the function you entered.')
-
-    return fn
-
-def error(message):
-    """Writes an html error message and exits"""
-    print html_start % (fn_str,tmin,tmax,tticks,ymin,ymax,yticks)
-    print "<p class='alert'>" + message + "</p>"
-    print footer
-    print html_end
-    sys.exit()
-
-def write_log(fn_str,(type,value,traceback),s):
-    if len(LOG_FILE) > 0:
-        f = open(LOG_FILE, 'a')
-        f.write("%s\t%s\t%s\t%s\n" % (fn_str,type,value,s))
-        f.close()
-
-html_start="""<!DOCTYPE html>
-<html>
-<head>
-<title>Slope Field</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-<meta name="viewport" content="width=800px"/>
-</head>
-
-<body>
-<h1>Slope Field by Nathan Grigg</h1>
-
-<form name="form" method="get">
-    <p>
-    <label><em>y'</em>(<em>t</em>,<em>y</em>) =
-        <input type="text" name="fn" size="50" value="%s"
-        autocorrect="off" autocapitalize="off">
-    </label>
-    <p> You may use: <tt>t y + - * / ^ e pi cos sin tan abs ln acos asin atan cosh sinh tanh</tt>
-    <p> Example: <tt>2*y/tan(2*t)</tt>
-    <p>
-    <table><tr>
-    <td><label>t-min:
-        <input type="number" name="tmin" size="5" value="%g">
-    </label></td>
-    <td><label>t-max:
-        <input type="number" name="tmax" size="5" value="%g">
-    </label></td>
-    <td><label>t-ticks:
-        <input type="number" name="tticks" size="5" value="%d"
-        min="10" max="40">
-    </label></td>
-    </tr><tr>
-    <td><label>y-min:
-        <input type="number" name="ymin" size=5 value="%g">
-    </label></td>
-    <td><label>y-max:
-        <input type="number" name="ymax" size=5 value="%g">
-    </label></td>
-    <td><label>y-ticks:
-        <input type="number" name="yticks" size=5 value="%d"
-        min="10" max="50">
-    </label></td>
-    </table>
-    <p><button type="submit">Draw the slope field</button>
-</form>
-"""
-
-footer = """<footer>
-<p>This page requires an up-to-date internet browser.
-<p>Want to host this on your own site and/or make changes?
-<a href="https://github.com/nathan11g/slopefield">Get
-the source from GitHub</a>.
-<p>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 License</a>.
-</footer>
-"""
-
-html_end="</body>\n</html>"""
-
-# begin execution
 
 cgitb.enable()
-print "Content-Type: text/html;\n"
 
-# extract values
-tmin = cgi_get("tmin",0)
-tmax = cgi_get("tmax",3)
-tticks = clip(cgi_get('tticks',40,int),10,50)
-
-ymin = cgi_get("ymin",-1)
-ymax = cgi_get("ymax",1)
-yticks = clip(cgi_get('yticks',30,int),10,40)
-
-# ensure that delta_t and delta_y will be positive
-if (tmax-tmin)/tticks <= 0 :
-    tmax = tmin + 1
-if (ymax-ymin)/yticks <= 0:
-    ymax = ymin + 1
-
-# get the function and sanitize
-
-fn_str=storage.getfirst("fn")
-
-if fn_str is None:
-    fn_str = ""
-    fn = lambda t,y: 2*y/tan(2*t)
-    title = "y'=2*y/tan(2*t)"
-else:
-    fn = sanitize(fn_str)
-    title = "y'=" + fn_str
-
-# print the graph
-
-print html_start % (fn_str,tmin,tmax,tticks,ymin,ymax,yticks)
-
-print '<div id="plot">'
-
-for line in slopefield.svg_slopefield(fn,tmin,tmax,ymin,ymax,tticks,yticks,
-  title=title):
-    print line
-
-print '</div>'
-
-print footer
-
-print html_end
+for line in slopefield.cgi_output(cgi_input,log_file=None):
+	print line
 
--- slopefield.py
+++ slopefield.py
@@ -1,3 +1,101 @@
+import re
+from math import sin,cos,tan,sqrt,e,pi,log,cosh,sinh,tanh
+
+VALID_WORDS = ['','sin','cos','tan','t','y','abs','sqrt','e','pi','log','ln',
+    'acos','asin','atan','cosh','sinh','tanh']
+ln = log
+
+class SanitizeError(Exception):
+    pass
+
+def parse_form(cgi_input,log_file=None):
+    """Returns dictionary with pertinent data from a FieldStorage instance"""
+
+    def cgi_get(name,default,convert=float):
+        """Gets the cgi value and converts it, with a default value on error"""
+        field_str = cgi_input.getfirst(name,str(default))
+        try:
+            value = convert(field_str)
+        except ValueError:
+            value = default
+
+        return value
+
+    tmin = cgi_get("tmin",0)
+    tmax = cgi_get("tmax",3)
+    tticks = clip(cgi_get('tticks',40,int),10,50)
+
+    ymin = cgi_get("ymin",-1)
+    ymax = cgi_get("ymax",1)
+    yticks = clip(cgi_get('yticks',30,int),10,40)
+
+    # ensure that delta_t and delta_y will be positive
+    if (tmax-tmin)/tticks <= 0 :
+        tmax = tmin + 1
+    if (ymax-ymin)/yticks <= 0:
+        ymax = ymin + 1
+
+    fn_str=cgi_input.getfirst("fn")
+
+    return {'tmin':tmin, 'tmax':tmax, 'tticks':tticks,
+            'ymin':ymin, 'ymax':ymax, 'yticks':yticks,
+            'fn_str': fn_str}
+
+def clip(value,left,right):
+    """Forces value to be within the range [left, right]"""
+    return min(right,max(left,value))
+
+def sanitize(fn_str,log_file=None):
+    """Sanitizes fn_str, evaluates and returns function"""
+    words = re.split(r'[0-9.+\-*/^ ()]+',fn_str)
+    for word in words:
+         if word not in VALID_WORDS:
+             raise SanitizeError(
+               'Unrecognized expression in function: %s' % word)
+
+    s = fn_str.replace('^','**')
+
+    # replace 1.232 with float(1.234)
+    s = re.sub(r'[0-9.]+', r'float(\g<0>)', s)
+
+    try:
+        fn = eval("lambda t,y: " + s)
+    except SyntaxError:
+        raise SanitizeError(
+        'Syntax Error. Something is wrong with the function you entered.\
+        Common mistakes include writing <tt>3x</tt> instead of <tt>3*x</tt> or\
+        <tt>sin t</tt> instead of <tt>sin(t)</tt>.')
+    except NameError as S:
+        write_log(log_file,fn_str,S,'eval')
+        raise SanitizeError(
+         'Something is wrong with the function you entered')
+    except Exception as S:
+        write_log(log_file,fn_str,S,'eval')
+        raise SanitizeError(
+          'Something is wrong with the function you entered.')
+
+    # sanity check
+    try:
+        output = fn(1.25,0.75)
+    except (ValueError, ZeroDivisionError, OverflowError):
+        pass
+    except Exception as S:
+        write_log(log_file,fn_str,S,'sanity check')
+        raise SanitizeError(
+          'Something is wrong with the function you entered.')
+
+    return fn
+
+def write_log(log_file,fn_str,msg,s):
+    if log_file is not None:
+        try:
+            f = open(log_file, 'a')
+        except:
+            pass
+        else:
+            f.write("%s\t%s\t%s\n" % (fn_str,msg,s))
+            f.close()
+
 class Canvas:
     def __init__(self,tmin=-1,tmax=1,ymin=-1,ymax=1,
       canvas_size=(400,400),title=""):
@@ -122,24 +220,24 @@ class Canvas:
             (self.yaxis_label_width*5,self.translate_y(value)+4,label)
         yield '</g>'
 
-def tick(x,y,f,length):
-    """Returns a tick centered at x,y with slope f(x,y) and given length"""
+def tick(t,y,f,length):
+    """Returns a tick centered at t,y with slope f(t,y) and given length"""
     try:
-        slope = f(x,y)
+        slope = f(t,y)
     except ZeroDivisionError:
         # vertical tick on division by zero
-        out = [x, y-0.5*length,
-               x, y+0.5*length]
+        out = [t, y-0.5*length,
+               t, y+0.5*length]
     except OverflowError:
-        out = [x, y-0.5*length,
-               x, y+0.5*length]
+        out = [t, y-0.5*length,
+               t, y+0.5*length]
     except:
-        out = [x,y,x,y]
+        out = [t,y,t,y]
     else:
         norm = (1+slope**2)**0.5
-        vx = 0.5 * length / norm
+        vt = 0.5 * length / norm
         vy = 0.5 * slope * length / norm
-        out = [x-vx, y-vy, x+vx, y+vy]
+        out = [t-vt, y-vy, t+vt, y+vy]
 
     return out
 
@@ -172,7 +270,97 @@ def svg_slopefield(f,tmin=-1,tmax=1,ymin=-1,ymax=1,tticks=20,yticks=20,
 
     yield canvas.svg_foot()
 
+html_start="""<!DOCTYPE html>
+<html>
+<head>
+<title>Slope Field</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=800px"/>
+</head>
 
-if __name__ == "__main__":
-    for line in svg_slopefield(lambda x,y: y):
-        print line
+<body>
+<h1>Slope Field by Nathan Grigg</h1>
+
+<form name="form" method="get">
+    <p>
+    <label><em>y'</em>(<em>t</em>,<em>y</em>) =
+        <input type="text" name="fn" size="50" value="%(fn_str)s"
+        autocorrect="off" autocapitalize="off">
+    </label>
+    <p> You may use: <tt>t y + - * / ^ e pi cos sin tan abs ln acos asin atan cosh sinh tanh</tt>
+    <p> Example: <tt>2*y/tan(2*t)</tt>
+    <p>
+    <table><tr>
+    <td><label>t-min:
+        <input type="number" name="tmin" size="5" value="%(tmin)g">
+    </label></td>
+    <td><label>t-max:
+        <input type="number" name="tmax" size="5" value="%(tmax)g">
+    </label></td>
+    <td><label>t-ticks:
+        <input type="number" name="tticks" size="5" value="%(tticks)d"
+        min="10" max="40">
+    </label></td>
+    </tr><tr>
+    <td><label>y-min:
+        <input type="number" name="ymin" size=5 value="%(ymin)g">
+    </label></td>
+    <td><label>y-max:
+        <input type="number" name="ymax" size=5 value="%(ymax)g">
+    </label></td>
+    <td><label>y-ticks:
+        <input type="number" name="yticks" size=5 value="%(yticks)d"
+        min="10" max="50">
+    </label></td>
+    </table>
+    <p><button type="submit">Draw the slope field</button>
+</form>
+"""
+
+html_end="""<footer>
+<p>This page requires an up-to-date internet browser.
+<p>Want to host this on your own site and/or make changes?
+<a href="https://github.com/nathan11g/slopefield">Get
+the source from GitHub</a>.
+<p>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 License</a>.
+</footer>
+</body>\n</html>
+"""
+
+
+def cgi_output(cgi_input,log_file=None):
+    """Generator for cgi output, one line at a time."""
+
+    yield "Content-Type: text/html;\n"
+
+    form = parse_form(cgi_input,log_file)
+
+    if form['fn_str'] is None:
+        form['fn_str'] = ""
+        form['fn'] = lambda t,y: 2*y/tan(2*t)
+        form['title'] = "y'=2*y/tan(2*t)"
+    else:
+        try:
+            form['fn'] = sanitize(form['fn_str'],log_file)
+        except SanitizeError as msg:
+            yield html_start % form
+            yield "<p class='alert'>" + msg + "</p>"
+            yield html_end
+            return
+        else:
+            form['title'] = "y'=" + form['fn_str']
+
+
+    # print the graph
+    yield html_start % form
+
+    yield '<div id="plot">'
+
+    for line in svg_slopefield(
+      form['fn'],form['tmin'],form['tmax'],form['ymin'],form['ymax'],
+      form['tticks'],form['yticks'],title=form['title']):
+        yield line
+
+    yield '</div>'
+
+    yield html_end
