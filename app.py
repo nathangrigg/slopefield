7cb66010f0b6dbf60bc513e6aff50141200b9882
Nathan Grigg <nathan@nathanamy.org>
2013-01-11 16:43:56 -0800

    Remove old index.cgi

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def graph():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
