50083da95f9dc061e3d4cffc6fa3b756d97f77fb
Nathan Grigg <nathan@nathanamy.org>
2013-02-26 19:43:21 -0800

    Pyflakes

--- slopefield.py
+++ slopefield.py
@@ -19,12 +19,15 @@ np.seterr(divide='ignore', invalid='ignore')
 # Methods to handle parameters
 ###########################################
 
+
 class SanitizeError(Exception):
     pass
+
 
 def clip(value, left, right):
     """Force value to be within the range [left, right]"""
     return min(right, max(left, value))
+
 
 def sanitize(params):
     """Sanitize the function and limit number of ticks"""
@@ -41,10 +44,11 @@ def sanitize(params):
         d['ymax'] = d['ymin'] + 1
 
     if d['fn_str']:
-        d['fn_str'] = re.sub(r'\bx\b', 't', d['fn_str']) # replace x with t
+        d['fn_str'] = re.sub(r'\bx\b', 't', d['fn_str'])  # replace x with t
         d['fn'] = sanitize_fn(d['fn_str'])
 
     return d
+
 
 def sanitize_fn(fn_str):
     """Sanitizes fn_str, evaluates and returns function.
@@ -65,8 +69,8 @@ def sanitize_fn(fn_str):
         fn = eval("lambda t,y: " + s)
     except SyntaxError:
         raise SanitizeError(
-        'Syntax Error. Something is wrong with the function you entered. ' + \
-        'Common mistakes include writing <tt>3t</tt> instead of <tt>3*t</tt> '+\
+        'Syntax Error. Something is wrong with the function you entered. ' +
+        'Common mistakes include writing <tt>3t</tt> instead of <tt>3*t</tt>' +
         ' or <tt>sin t</tt> instead of <tt>sin(t)</tt>.')
     except NameError as S:
         raise SanitizeError(
@@ -83,7 +87,7 @@ def sanitize_fn(fn_str):
     except TypeError as S:
         if S.message == "'float' object is not callable":
             raise SanitizeError(
-              'Invalid syntax. Please use explicit multiplication. ' +\
+              'Invalid syntax. Please use explicit multiplication. ' +
               '(Bad: 5y. Good: 5*y.)')
         else:
             raise SanitizeError(
@@ -99,10 +103,12 @@ def sanitize_fn(fn_str):
 # Methods to output svg
 ###########################################
 
+
 def svg_tick(tick):
     """Returns a line of svg representing the tick"""
     return '<line x1 = "%.01f" y1 = "%.01f" x2 = "%.01f" y2 = "%.01f" />' \
            % tuple(tick)
+
 
 def axes(canvas, trans, title=""):
     """Creates a box with axis labels"""
@@ -110,31 +116,31 @@ def axes(canvas, trans, title=""):
     # the box
     yield '<g style="stroke-width:1; stroke:grey;">'
     yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (canvas['left'], canvas['top'], canvas['left'], canvas['bottom']+2)
+        (canvas['left'], canvas['top'], canvas['left'], canvas['bottom'] + 2)
     yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (canvas['left'] - 2, canvas['bottom'],
-           canvas['right'], canvas['bottom'])
+        (canvas['left'] - 2, canvas['bottom'],
+         canvas['right'], canvas['bottom'])
     yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (canvas['right'], canvas['bottom']+2, canvas['right'], canvas['top'])
+        (canvas['right'], canvas['bottom'] + 2, canvas['right'], canvas['top'])
     yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (canvas['left']-2, canvas['top'], canvas['right'], canvas['top'])
+        (canvas['left'] - 2, canvas['top'], canvas['right'], canvas['top'])
 
     # the axis ticks
     for t in canvas['taxis'][1:-1]:
         yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (t*trans['tm']+trans['tb'], canvas['bottom']-2,
-           t*trans['tm']+trans['tb'], canvas['bottom']+2)
+          (t * trans['tm'] + trans['tb'], canvas['bottom'] - 2,
+           t * trans['tm'] + trans['tb'], canvas['bottom'] + 2)
         yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (t*trans['tm']+trans['tb'], canvas['top'],
-           t*trans['tm']+trans['tb'], canvas['top']+2)
+          (t * trans['tm'] + trans['tb'], canvas['top'],
+           t * trans['tm'] + trans['tb'], canvas['top'] + 2)
 
     for y in canvas['yaxis'][1:-1]:
         yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (canvas['left']-2, y*trans['ym']+trans['yb'],
-           canvas['left']+2, y*trans['ym']+trans['yb'])
+          (canvas['left'] - 2, y * trans['ym'] + trans['yb'],
+           canvas['left'] + 2, y * trans['ym'] + trans['yb'])
         yield '<line x1="%s" y1="%s" x2="%s" y2="%s" />' % \
-          (canvas['right']-2, y*trans['ym']+trans['yb'],
-           canvas['right'], y*trans['ym']+trans['yb'])
+          (canvas['right'] - 2, y * trans['ym'] + trans['yb'],
+           canvas['right'], y * trans['ym'] + trans['yb'])
 
     yield "</g>"
 
@@ -147,7 +153,7 @@ def axes(canvas, trans, title=""):
 
     # and the title
     yield '<text x="%s" y="%s">%s</text>' % \
-      ((canvas['right'] - canvas['top'])/2, canvas['top']-5, title)
+      ((canvas['right'] - canvas['top']) / 2, canvas['top'] - 5, title)
     yield '</g>'
 
     # the y axis labels
@@ -155,29 +161,31 @@ def axes(canvas, trans, title=""):
      'style="text-anchor:end;">'
     for value, label in zip(canvas['yaxis'], canvas['yaxis_label']):
         yield '<text x="%d" y="%s">%s</text>' % \
-        (canvas['left']-6, value*trans['ym'] + trans['yb'] + 4, label)
+        (canvas['left'] - 6, value * trans['ym'] + trans['yb'] + 4, label)
     yield '</g>'
+
 
 def canvas_dimensions(form, canvas_size=(750, 500)):
     """calculate dimensions of the canvas, making room for axes"""
     w, h = canvas_size
 
     # make axis ticks and labels
-    taxis = [form['tmin'] + float(form['tmax']-form['tmin'])*i/10 \
+    taxis = [form['tmin'] + float(form['tmax'] - form['tmin']) * i / 10
       for i in range(11)]
     taxis_label = ["%.3g" % t for t in taxis]
-    yaxis = [form['ymin'] + float(form['ymax']-form['ymin'])*i/10 \
+    yaxis = [form['ymin'] + float(form['ymax'] - form['ymin']) * i / 10
       for i in range(11)]
     yaxis_label = ["%.3g" % y for y in yaxis]
     yaxis_label_width = max([len(label) for label in yaxis_label])
 
     # adjust canvas for labels
     top, bottom = 15, h - 15
-    left, right = 6*yaxis_label_width+6, w-15
+    left, right = 6 * yaxis_label_width + 6, w - 15
 
     return {'top': top, 'left': left, 'bottom': bottom, 'right': right,
         'w': w, 'h': h, 'taxis': taxis, 'taxis_label': taxis_label,
         'yaxis': yaxis, 'yaxis_label': yaxis_label}
+
 
 def svg_slopefield(canvas, tick_array, **kwargs):
     """Returns an svg image for a slopefield"""
@@ -204,11 +212,13 @@ xmlns:xlink="http://www.w3.org/1999/xlink">""" % canvas
 # Methods to create tick array
 ###########################################
 
+
 def translation(p1, i1, p2, i2):
     """Returns the affine translation that takes p1 to i1 and p2 to i2"""
-    stretch = (i2-i1) / (p2-p1)
+    stretch = (i2 - i1) / (p2 - p1)
     shift = i1 - stretch * p1
     return stretch, shift
+
 
 def tick(t, y, f, length, trans):
     """Returns a tick centered at t,y with slope f(t,y) and given length"""
@@ -216,14 +226,14 @@ def tick(t, y, f, length, trans):
     yy = y * trans['ym'] + trans['yb']
     slope = f(t, y)
     # vector components
-    vt =  trans['tm']
-    vy =  trans['ym'] * slope
+    vt = trans['tm']
+    vy = trans['ym'] * slope
     # normalize
-    norm = (vt**2 + vy**2)**0.5
+    norm = (vt ** 2 + vy ** 2) ** 0.5
     vt *= 0.5 * length / norm
     vy *= 0.5 * length / norm
     # combine
-    out = np.vstack([tt-vt, yy-vy, tt+vt, yy+vy]).T
+    out = np.vstack([tt - vt, yy - vy, tt + vt, yy + vy]).T
 
     # fix infinite slopes
     inf_mask = np.isinf(slope)
@@ -234,10 +244,13 @@ def tick(t, y, f, length, trans):
     # delete NaNs
     return out[~np.isnan(slope)]
 
+
 def ticks_step(min, max, num_ticks):
     """Calculate steps and step size"""
-    ticks, step = np.linspace(min, max, num_ticks, retstep=True, endpoint=False)
+    ticks, step = np.linspace(min, max, num_ticks,
+      retstep=True, endpoint=False)
     return ticks + step / 2, step
+
 
 def slopefield(form, trans):
     """Returns a slopefield array"""
@@ -246,13 +259,16 @@ def slopefield(form, trans):
     yticks, dy = ticks_step(form['ymin'], form['ymax'], form['yticks'])
 
     ts, ys = np.meshgrid(tticks, yticks)
-    ticklength = 0.6 * min(abs(dt*trans['tm']), abs(dy*trans['ym']))
+    ticklength = 0.6 * min(abs(dt * trans['tm']), abs(dy * trans['ym']))
 
-    return tick(ts.flatten(), ys.flatten(), form['fn'], ticklength, trans=trans)
+    return tick(ts.flatten(), ys.flatten(), form['fn'],
+      ticklength, trans=trans)
 
 ###########################################
 # Methods to create html output
 ###########################################
+
+
 def html_output(params):
     """Generator for html output, one line at a time."""
 
@@ -268,7 +284,7 @@ def html_output(params):
 
     if not form['fn_str']:
         form['fn_str'] = ""
-        form['fn'] = lambda t, y: 2*y/tan(2*t)
+        form['fn'] = lambda t, y: 2 * y / tan(2 * t)
         form['title'] = "y'=2*y/tan(2*t)"
 
     # set up the canvas
@@ -290,8 +306,7 @@ def html_output(params):
 
     # print it out
 
-    start, end = TEMPLATE.safe_substitute(form)\
-      .split('$content',1)
+    start, end = TEMPLATE.safe_substitute(form).split('$content', 1)
 
     yield start
 
